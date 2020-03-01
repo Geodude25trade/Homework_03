6db098c7bb84122259e9e2513dd21024b2246d24
Geodude25trade <rockpounder@gmail.com>
2020-02-29 20:39:01 -0700

    Added app and apptest

---
+++ src/main/java/App.java
@@ -0,0 +1,16 @@
+public class App {
+    private int number;
+
+    public static void main(String[] args) {
+
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testNumber() {
+        App app = new App();
+        app.setNumber(4);
+        assertEquals(4, app.getNumber());
+    }
+
+}
